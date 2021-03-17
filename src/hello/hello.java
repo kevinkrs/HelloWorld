21a75dc1dbf24979e12921b8e1c58c9f51bf544b
ubptj <kevin.kraus@student.kit.edu>
2021-03-17 13:37:01 +0100

    Finaly

---
+++ src/hello/hello.java
@@ -0,0 +1,32 @@
+package hello;
+
+import java.util.Arrays;
+
+public class hello {
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array [] = {1,4,2,2,3,1,5,6,6,4,3};
+		arrayFunction(array);
+
+	}
+	
+	
+	
+	static void arrayFunction(int [] myArray) {
+		
+			Arrays.sort(myArray);
+			int i = 0;
+			System.out.println("Uniqe Element:");
+			while (i < myArray.length-1) {
+				if( myArray[i] != myArray[i+1]) {
+						System.out.print(" "+myArray[i]);
+				}
+				i = i+2;
+			}
+		}
+
+	}
